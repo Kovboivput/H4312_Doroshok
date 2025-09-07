3c888e3556ece425106ccf7627bc0b1c2721b7fe
Ivan <ivandoroshok123@gmail.com>
2025-09-07 13:08:28 +0200

    Nicks year

---
+++ para2/simulatorStudent.py
@@ -0,0 +1,57 @@
+from random import randint
+
+class Student:
+    def __init__(self, name):
+        self.name = name
+        self.gladness = 50
+        self.progress = 0
+        self.alive = True
+
+
+    def to_study(self):
+        print ("time to study")
+        self.progress += 0.12
+        self.gladness -= 3
+    def to_sleep(self):
+        print ("time to sleep")
+        self.gladness += 3
+    def to_chill(self):
+        print ("rest time")
+        self.gladness += 5
+        self.progress -= 0.1
+
+    def is_alive(self):
+        if self.progress < - 0.5:
+            print ("cast out")
+            self.alive = False
+        elif self.gladness <= 0:
+            print ("depression")
+            self.alive = False
+        elif self.progress > 5:
+            print ("Passed externally")
+            self.alive = False
+    def end_of_day (self):
+        print (f"gladness = {self.gladness}")
+        print (f"progress = {round(self.progress, 2)}")
+    def live(self, day):
+        text_day = f"day{day} of {self.name} live"
+        print (f"{text_day:=^30} ")
+        cube = randint(1, 3)
+        if cube == 1:
+            self.to_sleep()
+        elif cube == 2:
+            self.to_study()
+        else:
+            self.to_chill()
+        self.end_of_day()
+        self.is_alive()
+
+
+
+
+Nick = Student(name="Kick")
+for day in range(365):
+    if Nick.alive == False:
+        break
+    Nick.live(day)
+
